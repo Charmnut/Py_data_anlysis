bf0760a8d15c5a179c26a1bfb7bf6a2b7d986c09
Pong Deng <44459341+Charmnut@users.noreply.github.com>
2020-02-11 15:05:36 +0800

    push
    push

---
+++ ch02_function.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+def get_counts(sequence):
+    counts = {}
+    for x in sequence:
+        if x in counts:
+            counts[x] += 1
+        else:
+            counts[x] = 1
+    return counts
+
+
+def top_counts(count_dict, n=10):
+    value_key_pairs = [(count, tz) for tz, count in count_dict.items()]
+    value_key_pairs.sort()
+    return value_key_pairs[-n:]
+
+
+def p1():
+    return 1
+
+
+
